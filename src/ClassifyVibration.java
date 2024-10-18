eaed601bb2cc21ba0b0edad483695be20a7f6813
Sissi <jliu948@wisc.edu>
2024-10-18 15:31:16 -0700

    changed guessdLabel comparison

--- src/ClassifyVibration.java
+++ src/ClassifyVibration.java
@@ -116,47 +116,53 @@ public class ClassifyVibration extends PApplet {
 			String guessedLabel = classifier.classify(captureInstance(null));	
 			// Yang: add code to stabilize your classification result
 			text("classified as: " + guessedLabel, 20, 30);
-            //add some code to make the UI more visual 
+            // add some code to make the UI more visual 
 			
 			// C4 (middle C) = 60 D4=62 E4=64 F4=65 G4=67 A4=69 B=71 C5 = 72
 			try {
-				// Get a synthesizer (for output only) and open it
-				Synthesizer synthesizer = MidiSystem.getSynthesizer();
-				synthesizer.open();
-				// Get MIDI channels (output channels for playing notes)
-				MidiChannel[] channels = synthesizer.getChannels();
-				MidiChannel piano = channels[0]; // Channel 0 is a piano
-	
-				if(guessedLabel == "do - c"){
-					piano.noteOn(60, 80);  // Note 60 is Middle C, velocity 80
+				if(!guessedLabel.equals("quiet")){
+					// Get a synthesizer (for output only) and open it
+					Synthesizer synthesizer = MidiSystem.getSynthesizer();
+					synthesizer.open();
+					// Get MIDI channels (output channels for playing notes)
+					MidiChannel[] channels = synthesizer.getChannels();
+					MidiChannel piano = channels[0]; // Channel 0 is a piano
+		
+					if(guessedLabel.equals( "do - c")){
+						piano.noteOn(60, 80);  // Note 60 is Middle C, velocity 80
+					}
+					else if(guessedLabel.equals( "re - d")){
+						piano.noteOn(62, 80);  // Note 62 is Middle D, velocity 80
+					}
+					else if(guessedLabel.equals("mi - e")){
+						piano.noteOn(64, 80);  // Note 64 is Middle E, velocity 80
+					}
+					else if(guessedLabel.equals("fa - f")){
+						piano.noteOn(65, 80);  // Note 65 is Middle F, velocity 80
+					}
+					else if(guessedLabel.equals("so - g")){
+						piano.noteOn(67, 80);  // Note 67 is Middle G, velocity 80	
+					}
+					else if(guessedLabel.equals("la - a")){
+						piano.noteOn(69, 80);  // Note 69 is Middle A, velocity 80
+					}
+					else if(guessedLabel.equals("re - d")){
+						piano.noteOn(71, 80);  // Note 71 is Middle B, velocity 80
+					}		
+					// Close the synthesizer when done
+					Thread.sleep(1000);    // Play the note for 1 second
+					piano.noteOff(60);     // Turn off the note
+					synthesizer.close();
+					// System.out.println("Playing some notes!");
 				}
-				else if(guessedLabel == "re - d"){
-					piano.noteOn(62, 80);  // Note 62 is Middle D, velocity 80
-				}
-				else if(guessedLabel == "mi - e"){
-					piano.noteOn(64, 80);  // Note 64 is Middle E, velocity 80
-				}
-				else if(guessedLabel == "fa - f"){
-					piano.noteOn(65, 80);  // Note 65 is Middle F, velocity 80
-				}
-				else if(guessedLabel == "so - g"){
-					piano.noteOn(67, 80);  // Note 67 is Middle G, velocity 80	
-				}
-				else if(guessedLabel == "la - a"){
-					piano.noteOn(69, 80);  // Note 69 is Middle A, velocity 80
-				}
-				else if(guessedLabel == "re - d"){
-					piano.noteOn(71, 80);  // Note 71 is Middle B, velocity 80
-				}		
-				// Close the synthesizer when done
-				Thread.sleep(1000);    // Play the note for 1 second
-				piano.noteOff(60);     // Turn off the note
-				synthesizer.close();
 
 			} catch (MidiUnavailableException e) {
 				System.err.println("MIDI device is unavailable: " + e.getMessage());
 			} catch (InterruptedException e) {
 				System.err.println("Thread was interrupted: " + e.getMessage());
+			}
+			if(!guessedLabel.equals("quiet")){
+				System.out.println("Playing some notes!");
 			}
 		}else {
 			text(classNames[classIndex], 20, 30);

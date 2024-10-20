4707fff0b50bfddfb4add8fa55a0f82999d21515
Tengyou Xu <tengyoux@gmail.com>
2024-10-20 15:01:44 -0700

    change threshold to 0.015

--- src/ClassifyVibration.java
+++ src/ClassifyVibration.java
@@ -38,7 +38,7 @@ public class ClassifyVibration extends PApplet {
 	int classIndex = 0;
 	int dataCount = 0;
 
-	double magnitudeThreshold = 0.03;//0.05; // Adjust this value as needed
+	double magnitudeThreshold = 0.015;//0.05; // Adjust this value as needed
 
 	MLClassifier classifier;
 
